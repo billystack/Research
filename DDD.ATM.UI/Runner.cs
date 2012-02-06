732b25c829a9d7b0096814920467f0aa25331fea
Billy Stack <bs.stack@hotmail.com>
2012-02-06 23:15:22 +0000

    Started on Base presenter

---
+++ DDD.ATM.UI/Runner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Fohjin.DDD.BankApplication
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			// TODO: Will have application bootstrapping here
+
+			// TODO: Will be using model/view presenter pattern here
+
+			// TODO: Must figure out how to wire all view events with presenter event handlers
+		}
+	}
+}
---
+++ DDD.ATM.UI/Views/LoginForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace DDD.ATM.UI.Views
+{
+	partial class LoginForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cmdLogin = new System.Windows.Forms.Button();
+			this.lblUsername = new System.Windows.Forms.Label();
+			this.txtUsername = new System.Windows.Forms.TextBox();
+			this.txtPassword = new System.Windows.Forms.TextBox();
+			this.lblPassword = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// cmdLogin
+			// 
+			this.cmdLogin.Location = new System.Drawing.Point(193, 140);
+			this.cmdLogin.Name = "cmdLogin";
+			this.cmdLogin.Size = new System.Drawing.Size(75, 23);
+			this.cmdLogin.TabIndex = 0;
+			this.cmdLogin.Text = "Login";
+			this.cmdLogin.UseVisualStyleBackColor = true;
+			// 
+			// lblUsername
+			// 
+			this.lblUsername.AutoSize = true;
+			this.lblUsername.Location = new System.Drawing.Point(60, 54);
+			this.lblUsername.Name = "lblUsername";
+			this.lblUsername.Size = new System.Drawing.Size(61, 13);
+			this.lblUsername.TabIndex = 1;
+			this.lblUsername.Text = "Username :";
+			// 
+			// txtUsername
+			// 
+			this.txtUsername.Location = new System.Drawing.Point(168, 54);
+			this.txtUsername.Name = "txtUsername";
+			this.txtUsername.Size = new System.Drawing.Size(100, 20);
+			this.txtUsername.TabIndex = 2;
+			// 
+			// txtPassword
+			// 
+			this.txtPassword.Location = new System.Drawing.Point(168, 90);
+			this.txtPassword.Name = "txtPassword";
+			this.txtPassword.Size = new System.Drawing.Size(100, 20);
+			this.txtPassword.TabIndex = 4;
+			// 
+			// lblPassword
+			// 
+			this.lblPassword.AutoSize = true;
+			this.lblPassword.Location = new System.Drawing.Point(60, 90);
+			this.lblPassword.Name = "lblPassword";
+			this.lblPassword.Size = new System.Drawing.Size(59, 13);
+			this.lblPassword.TabIndex = 3;
+			this.lblPassword.Text = "Password :";
+			// 
+			// LoginForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(361, 209);
+			this.Controls.Add(this.txtPassword);
+			this.Controls.Add(this.lblPassword);
+			this.Controls.Add(this.txtUsername);
+			this.Controls.Add(this.lblUsername);
+			this.Controls.Add(this.cmdLogin);
+			this.Name = "LoginForm";
+			this.Text = "[ATM] - CQRS Demo";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button cmdLogin;
+		private System.Windows.Forms.Label lblUsername;
+		private System.Windows.Forms.TextBox txtUsername;
+		private System.Windows.Forms.TextBox txtPassword;
+		private System.Windows.Forms.Label lblPassword;
+	}
+}
