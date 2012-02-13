43f87f84b9c4530d36c089c3e23faa39ed79962c
Billy Stack <bs.stack@hotmail.com>
2012-02-13 23:23:10 +0000

    Added ActionDelegate...

---
+++ DDD.ATM.UI/Views/ActionDelegate.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+
+namespace DDD.ATM.UI.Views
+{
+	// Using this to avoid event ahandlers that have object and event args
+	public delegate void EventAction();
+}
---
+++ DDD.ATM.UI/Views/ILoginForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using DDD.ATM.UI.Presenters;
+
+
+namespace DDD.ATM.UI.Views
+{
+	public interface ILoginForm
+	{
+		void Display(
+			DDD.ATM.UI.Presenters.LoginPresenter subject);
+	}
+}
