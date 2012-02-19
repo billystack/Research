a82ef1300aac7f7e0b1440bd18aa72e6864bb5cb
Billy Stack <bs.stack@hotmail.com>
2012-02-19 13:32:45 +0000

    Added Commands assembly and created CustomerAttemptsLogin command

---
+++ DDD.ATM.Commands/CustomerAttemptingLogin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace DDD.ATM.Commands
+{
+	public class CustomerAttemptingLogin
+	{
+		private readonly string c_userName;
+		private readonly string c_password;
+
+		public string UserName { get { return this.c_userName; } }
+		public string Password { get { return this.c_password; } }
+
+
+		public CustomerAttemptingLogin(
+			string userName, 
+			string password)
+        {
+			this.c_userName = userName;
+			this.c_password = password;
+        }
+	}
+}
